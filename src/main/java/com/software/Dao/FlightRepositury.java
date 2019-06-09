fbc3b3d37d9c76e1ab8ea1728a679b521fb52127
xuan0-0 <986583688@qq.com>
2019-06-09 16:52:12 +0800

    database framework finshed

--- src/main/java/com/software/Dao/FlightRepositury.java
+++ src/main/java/com/software/Dao/FlightRepositury.java
@@ -2,6 +2,7 @@ package com.software.Dao;
 
 import com.software.Domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
